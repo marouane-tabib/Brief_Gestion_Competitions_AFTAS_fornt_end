727a6c641e764bcd449b5a1fce016be66c13c79a
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-21 09:01:53 +0100

    AFTAS-2 | Add new route, with cometitions path and CometitionsComponent as a component - app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { CompetitionsComponent } from "./Modules/competitions/competitions.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: CompetitionsComponent
+  },
+  {
+    path: 'competitions', component: CompetitionsComponent
+  }
+];
