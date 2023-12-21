1091caa5fb63e44e6270b22c64759bb61602c0f1
marwan-tabib <marwanmaroc297@gmail.com>
2023-12-21 22:24:44 +0100

    AFTAS-2 | Make Modules/competitions derictory

---
+++ src/app/Modules/competitions/competitions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competitions',
+  standalone: true,
+  imports: [],
+  templateUrl: './competitions.component.html',
+  styleUrl: './competitions.component.scss'
+})
+export class CompetitionsComponent {
+
+}
